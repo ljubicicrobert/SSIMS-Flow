393a4537737b84b72a6d82b05063bc17513c23cf
ljubicicrobert <ljubicicrobert@gmail.com>
2022-05-30 14:40:45 +0200

    Update filtering.cpp

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -8,7 +8,7 @@ __package_name__ = 'SSIMS-Flow: UAV image velocimetry workbench'
 __description__ = 'Workbench for obtaining open-channel flow from UAV videos using dense optical flow'
 __version__ = '0.1.0.5'
 __status__ = 'beta'
-__date_deployed__ = '2022-05-27'
+__date_deployed__ = '2022-05-30'
 
 __author__ = 'Robert Ljubicic, University of Belgrade - Civil Engineering Faculty'
 __author_email__ = 'rljubicic@grf.bg.ac.rs'
