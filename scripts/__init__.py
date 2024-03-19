919b9ede007d367f1f6ad04789b76b12586b0587
ljubicicrobert <ljubicicrobert@gmail.com>
2024-03-19 16:54:35 +0100

    Migrate to .NET Framework 4.7.2

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -6,9 +6,9 @@ from argparse import ArgumentParser
 
 __package_name__ = 'SSIMS-Flow: UAV image velocimetry workbench'
 __description__ = 'Workbench for obtaining open-channel flow from UAV videos using dense optical flow'
-__version__ = '0.5.3.3'
+__version__ = '0.5.4.0'
 __status__ = 'beta'
-__date_deployed__ = '2024-03-13'
+__date_deployed__ = '2024-03-19'
 
 __author__ = 'Robert Ljubicic @ Faculty of Civil Engineering, University of Belgrade'
 __author_email__ = 'rljubicic@grf.bg.ac.rs, ljubicicrobert@gmail.com'
